4d4b91dad0e25746b35e6c76bf44481577955a62
Zhixia Lin <linzhixia@berkeley.edu>
2025-02-21 15:40:38 -0800

    add more fake driver data

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,5 +1,5 @@
 class Driver < ApplicationRecord
-    validates :name, :phone, :shifts, :active, presence: true
-    validates :email, presence: false
+    validates :name, :phone, :shifts, presence: true
+    validates :email, :active, presence: false
   # validates :phone, format: { with: /\A\(\d{3}\)\d{3}-\d{3}\z/, message: "must be in format (xxx)xxx-xxx" }
 end
