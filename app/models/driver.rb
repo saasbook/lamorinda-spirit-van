b6bc59d595c5158b89fc743347e0fcbf0b86b91f
Daniel Tan <daniel_tan@berkeley.edu>
2025-02-21 14:58:28 -0800

    linter fix

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,5 +1,5 @@
 class Driver < ApplicationRecord
     validates :name, :phone, :shifts, :active, presence: true
     validates :email, presence: false
-    # validates :phone, format: { with: /\A\(\d{3}\)\d{3}-\d{3}\z/, message: "must be in format (xxx)xxx-xxx" }
+  # validates :phone, format: { with: /\A\(\d{3}\)\d{3}-\d{3}\z/, message: "must be in format (xxx)xxx-xxx" }
 end
--- db/seed_data.rb
+++ db/seed_data.rb
@@ -1,5 +1,4 @@
 module SeedData
-
   def self.drivers
     [
       {
