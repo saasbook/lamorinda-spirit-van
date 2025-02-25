4b03214e214138607c931337ab77d87f2e6ad96e
Zhixia Lin <linzhixia@berkeley.edu>
2025-02-24 16:41:25 -0800

    reformat to pass rubocop

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -6,7 +6,7 @@ class Ride < ApplicationRecord
   scope :filter_by_driver, ->(driver_name) {
     where("driver LIKE ?", "%#{driver_name}%") if driver_name.present?
   }
-  
+
   def self.filtered_rides(driver_name)
     today.filter_by_driver(driver_name)
   end
