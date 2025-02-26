91893956ab84ba7f27cebf9cda8b4f4caf4d88a2
Zhixia Lin <linzhixia@berkeley.edu>
2025-02-25 19:12:49 -0800

    fix the .gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.2].define(version: 2025_02_24_022846) do
-  create_table "rides", force: :cascade do |t|
-    t.string "day", null: false
-    t.date "date", null: false
-    t.text "driver"
-    t.integer "van"
-    t.text "passenger_name_and_phone"
-    t.text "passenger_address"
-    t.text "destination"
-    t.text "notes_to_driver"
-    t.string "driver_initials"
-    t.float "hours"
-    t.decimal "amount_paid", precision: 10, scale: 2
-    t.integer "ride_count"
-    t.string "c"
-    t.text "notes_date_reserved"
-    t.text "confirmed_with_passenger"
-    t.string "driver_email"
+  create_table "drivers", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "email"
+    t.json "shifts"
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
-end
-ActiveRecord::Schema[7.2].define(version: 2025_02_23_113345) do
+
   create_table "passengers", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
@@ -51,5 +40,24 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_23_113345) do
     t.text "audit"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.string "day", null: false
+    t.date "date", null: false
+    t.text "driver"
+    t.integer "van"
+    t.text "passenger_name_and_phone"
+    t.text "passenger_address"
+    t.text "destination"
+    t.text "notes_to_driver"
+    t.string "driver_initials"
+    t.float "hours"
+    t.decimal "amount_paid", precision: 10, scale: 2
+    t.integer "ride_count"
+    t.string "c"
+    t.text "notes_date_reserved"
+    t.text "confirmed_with_passenger"
+    t.string "driver_email"
   end
 end
