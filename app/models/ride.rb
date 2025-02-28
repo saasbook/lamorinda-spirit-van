5f32c49475451d48687b1d2038470d5afe4d8e54
Daniel Tan <daniel_tan@berkeley.edu>
2025-02-27 16:35:33 -0800

    model clean up

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,9 +1,16 @@
 class Ride < ApplicationRecord
   validates :day, :date, :passenger_name_and_phone, presence: true
 
-  def self.filtered_rides(driver_name)
-    rides = where(date: Time.zone.today)
+  def self.today_rides(rides)
+    rides.where(date: Time.zone.today)
+  end
+
+  def self.rides_by_driver(rides, driver_name)
     rides = rides.where("driver LIKE ?", "%#{driver_name}%") if driver_name.present?
     rides
+  end
+
+  def self.filtered_rides(driver_name)
+    self.rides_by_driver(self.today_rides(Ride.all), driver_name)
   end
 end
