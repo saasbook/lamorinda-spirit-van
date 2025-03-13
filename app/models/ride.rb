2f7e682b01aed93d85e087b74decd359e031ce7d
AthenaJenkins <athena.j.jenkins@gmail.com>
2025-03-13 16:06:49 -0700

    Fixed association errors, updated fake data

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,8 +3,8 @@
 class Ride < ApplicationRecord
   belongs_to :passenger, optional: true
   belongs_to :driver
-  has_one :start_address, class: 'Address', foreign_key: :start_address_id
-  has_one :dest_address, class: 'Address', foreign_key: :dest_address_id
+  belongs_to :start_address, class_name: 'Address', foreign_key: :start_address_id
+  belongs_to :dest_address, class_name: 'Address', foreign_key: :dest_address_id
 
 
   # def self.today_rides(rides)
