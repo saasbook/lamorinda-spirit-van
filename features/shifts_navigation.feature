9edcbeed4231f5ed858d7bf715843627ca389193
Zhixia Lin <linzhixia@berkeley.edu>
2025-03-20 21:47:17 -0700

    add cucumber test for Read-Only

--- features/shifts_navigation.feature
+++ features/shifts_navigation.feature
@@ -1,7 +1,7 @@
 Feature: Navigation from Homepage to Shifts Calendar
 
   Scenario: User navigates from "homepage" to "Today's Rides"
-    Given I am on the "homepage"
+    Given I am on the "homepage" page
     When I click on "Today's Rides" button
     Then I should be on the "Today's Rides" page
     
@@ -10,4 +10,16 @@ Feature: Navigation from Homepage to Shifts Calendar
     Then I should see "View Shifts" button
     When I click on "View Shifts" button
     Then I should be on the "Read-Only Shift Calendar" page
+
+  Scenario: User click button to switch month
+    Given I am on the "Read-Only Shift Calendar" page
+    Then I should see the current month and year in the calendar title
+    And I should see "Last Month" "Jump to Today" "Next Month" button
+    When I note the current month title
+    When I click on "Last Month" button
+    Then I should see the month title change
+    When I click on "Jump to Today" button
+    Then I should see the current month title again
+    When I click on "Next Month" button
+    Then I should see the month title change
 
--- features/step_definitions/shifts_navigation_steps.rb
+++ features/step_definitions/shifts_navigation_steps.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-Given("I am on the homepage") do
-  visit root_path
-end
-
 Given("I am on the {string} page") do |page_name|
   case page_name
+  when "homepage"
+    visit root_path
   when "Today's Rides"
     visit today_rides_path
   when "Read-Only Shift Calendar"
@@ -20,18 +18,47 @@ When("I click on {string} button") do |button_text|
 end
 
 Then("I should be on the {string} page") do |page_name|
-  case page_name
-  when "Today's Rides"
-    expect(current_path).to eq today_rides_path
-  when "Read-Only Shift Calendar"
-    expect(current_path).to eq read_only_shifts_path
-  else
-    raise "Unknown page name: #{page_name}"
+  expected_path = case page_name
+                  when "Today's Rides"
+                    today_rides_path
+                  when "Read-Only Shift Calendar"
+                    read_only_shifts_path
+                  else
+                    raise "Unknown page name: #{page_name}"
   end
+
+  expect(current_path).to eq expected_path
 end
 
 Then("I should see {string} button") do |button_text|
   # search button or link
   found = page.has_button?(button_text) || page.has_link?(button_text)
   expect(found).to be true
+end
+
+Then("I should see the current month and year in the calendar title") do
+  expected_title = Time.zone.today.strftime("%B %Y") # e.g., "March 2025"
+  actual_title = find(".calendar-title").text
+  expect(actual_title).to eq(expected_title)
+end
+
+Then("I should see {string} {string} {string} button") do |btn1, btn2, btn3|
+  [btn1, btn2, btn3].each do |btn|
+    found = page.has_button?(btn) || page.has_link?(btn)
+    expect(found).to be true
+  end
+end
+
+When("I note the current month title") do
+  @current_month = find(".calendar-title").text
+end
+
+Then("I should see the month title change") do
+  new_month = find(".calendar-title").text
+  expect(new_month).not_to eq(@current_month)
+end
+
+Then("I should see the current month title again") do
+  current_month = find(".calendar-title").text
+  expect(current_month).to eq(@current_month)
 end
