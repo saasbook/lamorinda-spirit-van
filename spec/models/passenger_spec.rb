75c62d81eacf50a5a8764f54afdae6a7c2aeae97
XZhaoF <xuefang958@gmail.com>
2025-04-02 19:16:56 -0700

    Added RSpecs tests for assocating existing addresses when creating or updating passengers

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -13,4 +13,54 @@ RSpec.describe Passenger, type: :model do
       expect(@passenger1.hispanic?).to eq(true)
     end
   end
+
+  describe "address assocations changes" do
+    context "when deleting a passenger record" do
+      it "does not delete the associated address record" do
+        existing_address_id = @passenger1.address_id
+        expect {@passenger1.destroy}.to change(Passenger, :count).by(-1)
+        expect(Address.exists?(existing_address_id)).to be true
+      end
+    end
+
+    context "when updating a passenger with a unique address" do
+      it "creates a new address record and associates it" do
+        original_address_id = @passenger2.address_id
+
+        new_attrs = {
+          street: "456 Unique St",
+          city: "Newville",
+          state: "CA",
+          zip: "99999"
+        }
+
+        expect {
+          @passenger2.update(address_attributes: new_attrs)
+        }.to change(Address, :count).by(1)
+
+        expect(@passenger2.address.street).to eq("456 Unique St".titleize)
+        expect(@passenger2.address.id).not_to eq(original_address_id)
+      end
+    end
+
+    context "when updating a passenger with an already existing address record" do
+      it "reuses existing address when updated with matching address fields" do
+        existing_address = create(:address, street: "123 Main St", city: "Orinda", state: "CA", zip: "94563")
+        passenger = create(:passenger)
+      
+        expect {
+          passenger.update(address_attributes: {
+            street: "123 Main St",
+            city: "Orinda",
+            state: "CA",
+            zip: "94563"
+          })
+        }.not_to change(Address, :count)
+      
+        expect(passenger.reload.address_id).to eq(existing_address.id)
+      end
+    end
+  end
 end
+
+
