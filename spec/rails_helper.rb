3ed12232d9cc6efa7bf215d62fa6eb5c5d8d9971
Zhixia Lin <linzhixia@berkeley.edu>
2025-04-03 12:38:02 -0700

    add HTML report version

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,22 @@
 # frozen_string_literal: true
 
 require_relative "support/factory_bot.rb"
+
 require "simplecov"
 require "simplecov_json_formatter"
-SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
-SimpleCov.start
+
+# Configure SimpleCov to generate both HTML and JSON reports.
+# HTML is useful for local inspection; JSON is used by Codecov or other CI tools.
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,      # Generates coverage/index.html
+  SimpleCov::Formatter::JSONFormatter       # Generates coverage/coverage.json for CI
+])
+
+SimpleCov.start "rails" do
+  # enable_coverage :branch         # Optional: also track branch coverage
+  add_filter "/spec/"            # Exclude spec files from coverage calculation
+end
+
 
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
