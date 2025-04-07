c94dcf983b99af43c3cd92f919d52c62ac255487
Zhixia Lin <linzhixia@berkeley.edu>
2025-04-06 22:04:56 -0700

    fix login, however there are duplicate code in features/step_definitions/logged_in_steps.rb and user_authentication_steps.rb

--- features/step_definitions/logged_in_steps.rb
+++ features/step_definitions/logged_in_steps.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 Given(/^I am logged in as a dispatcher$/) do
+  @dispatcher = FactoryBot.create(:user,
+                                  :dispatcher,
+                                  email: "dispatcher1@example.com",
+                                  password: "password")
+  visit new_user_session_path
+
+  fill_in "Email", with: @dispatcher.email
+  fill_in "Password", with: @dispatcher.password
+  click_button "Log in"
+
+  expect(page).not_to have_content("Log in")
+  expect(page).to have_title("Lamorinda")
 end
