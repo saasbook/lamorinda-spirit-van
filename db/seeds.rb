871e58ed557e4136e15e4625416403547b48e070
AthenaJenkins <athena.j.jenkins@gmail.com>
2025-04-08 14:10:00 -0700

    Added shifts for the next 60 days to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,35 @@ SeedData.drivers.each do |driver|
   Driver.create(driver)
 end
 
-User.create!(
-  email: "admin@example.com",
-  password: "password",
-  role: "admin"
-)
+if User.all.empty?
+  User.create!(
+    email: "admin@example.com",
+    password: "password",
+    role: "admin"
+  )
 
-User.create!(
-  email: "dispatcher@example.com",
-  password: "password",
-  role: "dispatcher"
-)
+  User.create!(
+    email: "dispatcher@example.com",
+    password: "password",
+    role: "dispatcher"
+  )
 
-User.create!(
-  email: "driver@example.com",
-  password: "password",
-  role: "driver"
-)
+  User.create!(
+    email: "driver@example.com",
+    password: "password",
+    role: "driver"
+  )
+end
+
+
+(Date.today.upto(Date.today + 60)).each do |day|
+  drivers = Driver.all
+  driver_id_am = day.mday % 6
+  driver_id_pm = (day.mday + 1) % 5
+  if day.wday in (1..5)
+    if Shift.where(shift_date: day).empty?
+      Shift.create!(shift_date: day, shift_type: "am", driver: drivers[driver_id_am])
+      Shift.create!(shift_date: day, shift_type: "pm", driver: drivers[driver_id_pm])
+    end
+  end
+end
