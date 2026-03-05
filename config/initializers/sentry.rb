01570f6c30e535a800b409a900cfb9b02ab6054d
Jiajun Liu <jliu618@berkeley.edu>
2026-03-04 22:31:15 -0800

    fixing lint

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.dsn = 'https://868909cbd4cb584e373e0d645a139488@o4510989335855104.ingest.us.sentry.io/4510989337034752'
+  config.dsn = "https://868909cbd4cb584e373e0d645a139488@o4510989335855104.ingest.us.sentry.io/4510989337034752"
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Add data like request headers and IP for users,
