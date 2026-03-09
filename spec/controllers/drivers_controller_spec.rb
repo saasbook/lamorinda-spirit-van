e4fe2c43a1bb1f2788e73a73a55a91032f34f2d5
n1qqq <n1qqq@foxmail.com>
2026-03-09 04:33:01 -0700

    Hardcode status to be not Waitlisted/Cancelled in controller tests

--- spec/controllers/drivers_controller_spec.rb
+++ spec/controllers/drivers_controller_spec.rb
@@ -15,9 +15,9 @@ RSpec.describe DriversController, type: :controller do
 
     @passenger1 = FactoryBot.create(:passenger)
     @passenger2 = FactoryBot.create(:passenger)
-    @ride1 = FactoryBot.create(:ride, driver: @driver1, passenger: @passenger1)
-    @ride2 = FactoryBot.create(:ride, driver: @driver2, passenger: @passenger1)
-    @ride3 = FactoryBot.create(:ride, driver: @driver1, passenger: @passenger2)
+    @ride1 = FactoryBot.create(:ride, driver: @driver1, passenger: @passenger1, status: "Scheduled")
+    @ride2 = FactoryBot.create(:ride, driver: @driver2, passenger: @passenger1, status: "Requested")
+    @ride3 = FactoryBot.create(:ride, driver: @driver1, passenger: @passenger2, status: "Pending")
     @ride4 = FactoryBot.create(:ride, driver: @driver1, passenger: @passenger1, date: Time.zone.tomorrow)
     @ride5 = FactoryBot.create(:ride, driver: @driver1, passenger: @passenger1, date: Time.zone.yesterday)
   end
