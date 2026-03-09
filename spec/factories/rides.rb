2d7619a4c725b24dc30831a89f5227176dfa5fb1
n1qqq <n1qqq@foxmail.com>
2026-03-09 04:20:11 -0700

    Revive date and appointment_time fields and their defaults

--- spec/factories/rides.rb
+++ spec/factories/rides.rb
@@ -2,14 +2,16 @@
 
 FactoryBot.define do
   factory :ride do
+    date { Date.current }
+    appointment_time { Time.current }
+    hours { rand(0.5..3.0).round(2) }
+    fare_amount { rand(5.0..50.0).round(2) }
+    amount_paid { rand(5.0..50.0).round(2) }
     van { rand(0..10) }
     ride_type { ["One-way", "Round-trip"].sample }
     fare_type { ["R", "LMV", "CC", "Shopping"].sample }
     status { ["Scheduled", "Requested", "Waitlisted", "Email sent", "Cancelled", "Confirmed", "Pending"].sample }
     source { ["Phone", "Email"].sample }
-    hours { rand(0.5..3.0).round(2) }
-    fare_amount { rand(5.0..50.0).round(2) }
-    amount_paid { rand(5.0..50.0).round(2) }
     wheelchair { [true, false].sample }
     disabled { [true, false].sample }
     need_caregiver { [true, false].sample }
