d7d47627417e43310269928528493420fe0a571a
n1qqq <n1qqq@foxmail.com>
2026-03-16 10:10:11 -0700

    add mock auth in spec/support/omniauth.rb before each block

--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -3,6 +3,11 @@
 RSpec.configure do |config|
   config.before(:each) do
     OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:entra_id] = OmniAuth::AuthHash.new({
+      provider: 'entra_id',
+      uid: '12345',
+      info: { email: 'dispatcher@example.com' }
+    })
   end
 
   config.after(:each) do
